54c21e0e4802165a1d73c4eccd60b3b665d35af4
DavidCorzo <davidcorzo@ufm.edu>
2019-09-23 22:15:03 -0600

    TERMINADO

--- main.py
+++ main.py
@@ -7,22 +7,22 @@ def main():
 
     lenght_of_args = len(sys.argv)
     # print(lenght_of_args)
-    # try:
-    if lenght_of_args == 1:
-        fun_dgcm.ALL()
+    try:
+        if lenght_of_args == 1:
+            fun_dgcm.ALL()
 
-    elif lenght_of_args != 1:
-        if sys.argv[1] == '1':
-            fun_dgcm.ONE()
-        elif sys.argv[1] == '2':
-            fun_dgcm.TWO()
-        elif sys.argv[1] == '3':
-            fun_dgcm.THREE()
-        elif sys.argv[1] == '4':
-            fun_dgcm.FOUR()
-    # except:
-    #     print(
-    #         f'Command line argument {sys.argv[1]} is not valid or is incorrectly written.')
+        elif lenght_of_args != 1:
+            if sys.argv[1] == '1':
+                fun_dgcm.ONE()
+            elif sys.argv[1] == '2':
+                fun_dgcm.TWO()
+            elif sys.argv[1] == '3':
+                fun_dgcm.THREE()
+            elif sys.argv[1] == '4':
+                fun_dgcm.FOUR()
+    except:
+        print(
+            f'Command line argument {sys.argv[1]} is not valid or is incorrectly written.')
 
 
 if __name__ == "__main__":
